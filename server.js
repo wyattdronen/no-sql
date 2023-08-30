0eaad4026ff6761c7741ca4928611aef4f7772b2
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-08-30 14:16:40 -0500

    mongo

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
-const db = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(routes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB Connected...');
+    })
+    .catch(err => console.log(err));
+
 
 db.once('open', () => {
     app.listen(PORT, () => {
